a2fc2018db7e9b4180200fbb21738cafe2f84b58
unknown <mohitshinde26601@gamil.com>
2025-06-10 19:45:34 +0530

    variable

---
+++ variables.js
@@ -0,0 +1,32 @@
+// const firstNameee = "Rohit"
+// console.log(firstNameee)
+
+
+
+// let firstNamee = "500"
+// console.log(firstNamee)
+
+// var firstName = "mohit";
+// console.log(firstName)
+
+// var a = 5;
+// var b = 6;
+// var c = a + b;
+
+// console.log(c)
+
+
+// var a = 100;
+// var a = 200;
+
+// console.log(a);
+
+// let a = 500;
+// a = 400;
+//  console.log(a);
+
+
+// const a = "lasdjf";
+// a = 600;
+
+// console.log(a)
