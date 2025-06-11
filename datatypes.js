4d8e2f3bdd2aeee5d1fdf798cbf1bfce338ecf9a
unknown <mohitshinde26601@gamil.com>
2025-06-11 20:38:39 +0530

    11 june

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+// string
+
+// let firstName = "virat";
+// let lastName = "kohli";
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+let a = 500;
+b = 400;
+ console.log(a);
---
+++ string.js
@@ -0,0 +1,81 @@
+// let firstName = "mohit";
+
+// let res = firstName.length
+// console.log(res);
+
+
+// let str = "          srting         ";
+// console.log(str);
+
+// let res = str.trim()
+// console.log(str);
+
+
+// ..........to uppercase...........
+// let Fname = "rahul";
+
+// let res = Fname.toUpperCase()
+
+// console.log(res);
+
+// ..........to loowercase...........
+// let Fname = "RAHUL";
+
+// let res = Fname.toLowerCase()
+
+// console.log(res);
+
+
+// .........indexof...........
+// let str = 'i love coding';
+// let res = str.indexOf("coding")
+
+// console.log(res);
+
+
+// // .........method chaining.........
+
+// let studentName = "           mangesh";
+// let res = studentName.trim().toUpperCase();
+// console.log(res);
+
+
+// ............slice..........
+
+// let str = "i lovey coding";
+// let res = str.slice(6, 7);
+// console.log(res);
+
+// //............replace.............
+
+// let str = "i love codding";
+// let res = str.replace("love","like");
+// console.log(res);
+
+
+
+
+// // ..........repeat.........
+// let str = "i love coding"
+// let res = str.repeat(5)
+// console.log(res);
+
+
+// let str = new String ("i love coding")
+// console.log(str);
+
+
+
+// let str = 'chetan'
+// console.log(typeof str);
+
+
+let a = 500;
+let b = 100;
+
+let str = `This is string Literals ${a * b}
+   
+     value of a is ${a} and value of b ${b}
+
+`
+console.log(str);
