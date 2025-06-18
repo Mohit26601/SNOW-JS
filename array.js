4f8119c97a4ca9dc696a7f468b1b30c2c72ff61e
unknown <mohitshinde26601@gamil.com>
2025-06-18 10:06:00 +0530

    array

---
+++ array.js
@@ -0,0 +1,110 @@
+//......... array.........
+
+
+// let car1 = "BMW";
+// let car2 = "volvo";
+// let car3 = "audi";
+
+// // ........1typeof array
+// let cars = ["BMW", "volvo", "audi"]
+// console.log(cars);
+
+
+// // ........2typeof array
+// let myArr = new Array("BMW", "volvo", "audi");
+// console.log(myArr);
+
+
+// // ........3typeof array
+// let emptyArray = [];
+// // emptyArray = 1;
+
+// emptyArray[0] = "abc";
+// emptyArray[1] = "def";
+// emptyArray[2] = "ghi";
+// emptyArray[3] = "jkl";
+// emptyArray[4] = "nop";
+
+// console.log(emptyArray[3]);
+
+// let arr = ["one", "two", "three", "four", "five", "six"];
+// console.log(arr.length);
+
+// ..........array push
+
+// let arr = ["one", "two", "three", "four", "five", "six"];
+// let res = arr.push("seven");
+// arr.push("eight");
+// arr.push("nine");
+// console.log(arr);
+
+
+// let arr = ["one", "two", "three", "four", "five", "six"];
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop()
+
+// console.log(arr);
+// arr.pop()
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+// console.log(arr);
+// arr.pop();
+
+
+// // .........To STRING
+// let arr = ["one", "two", "three", "four", "five", "six"];
+// console.log(arr.toString())
+
+
+// // ...........concat
+
+// let arr1 = ["one", "two", "three", "four", "five", "six"];
+// let arr2 = ["seven", "eight", "nine", "ten", "eleven", "twelve"];
+
+// let res = arr1.concat(arr2);
+
+// console.log(res);
+
+
+
+// // ..............unshift
+
+// let arr1 = ["one", "two", "three", "four", "five", "six"];
+
+// let res = arr1.unshift("seven");
+// console.log(arr1);
+
+// let arr2 = ["one", "two", "three", "four", "five", "six"];
+
+// arr2.shift()
+// console.log(arr2);
+
+
+// // .............slilce
+
+// let arr = ["one", "two", "three", "four", "five", "six"];
+
+// console.log( arr.slice('1', '3'));
+
+
+// ..............splice
+
+let arr1 = ["one", "two", "three", "four", "five", "six", "seven", "eight"];
+console.log(arr1.splice('4', '4'));
+
+
+
+
+
