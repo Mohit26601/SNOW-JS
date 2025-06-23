4614c170bcb2b53ad221f2b1c2637f6c7d19c61e
unknown <mohitshinde26601@gamil.com>
2025-06-23 19:34:18 +0530

    functions

--- functions.js
+++ functions.js
@@ -5,19 +5,19 @@
 
 
 
-function myFunc(theObject) {
-    theObject.make = "Toyota";
-  }
+// function myFunc(theObject) {
+//     theObject.make = "Toyota";
+//   }
   
-  const myCar = {
-    make: "Honda",
-    model: "Accord",
-    year: 1998,
-  };
+//   const myCar = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 1998,
+//   };
   
-  console.log(myCar.make); // "Honda"
-  myFunc(myCar);
-  console.log(myCar.make); // "Toyota"
+//   console.log(myCar.make); // "Honda"
+//   myFunc(myCar);
+//   console.log(myCar.make); // "Toyota"
 
 
 
@@ -26,8 +26,124 @@ function myFunc(theObject) {
   
 // this is how we call the function in js
 
-function addValue(){
-    console.log("This is js");
+// function addValue(){
+//     console.log("This is js");
+// }
+
+// addValue();
+
+
+
+// function mohit(){
+//   console.log("mohit shinde");
+  
+// }
+// mohit();
+
+// function addValue(){
+
+//  let a = 100;
+//  let b = 200;
+//  let res = a + b;
+
+
+// }
+//addValue();
+
+
+
+// function addValue(){
+//    let a = 100;
+//    let b = 200;
+//    let res = a + b;
+//   console.log(res);
+//  }
+  
+//    addValue();
+
+
+
+  //  function addValue(){
+
+  //   let a = 100;
+  //   let b = 200;
+  //   let res = a + b;
+  //   console.log("This is before return");
+  //   return a + b;
+  //   console.log(res);
+  //  }
+   
+   
+
+  //  console.log(addValue());
+   
+
+
+// function addValue(num1, num2){       //parameters
+//   console.log(num1 + num2);
+  
+// }
+// addValue(100, 200);           //arguments
+
+
+
+// function addValue(num1, num2)
+// {       //parameters
+//   return num1 + num2;
+  
+// }
+        
+// console.log (addValue(100, 800));              //arguments
+
+
+
+// function getValue(...num1)
+// {
+//   return num1
+
+// }
+// console.log(getValue(200, 300, 400, 500));
+
+
+function infoCode(){
+  let name = "Tushar";
+  let age = 24;
+  let stuObj ={
+    student : "rohan"
+  }
+  let arr = [1,2,3,4,5]
+  console.log(name, age,stuObj,arr);
+
+  
 }
 
-addValue();
+infoCode();
+
+
+// // ..... We are accessing the object though function in below code        ........ object values are access by function .......
+
+// let userOne ={
+//   userName : "pranit",                        // object 
+//   userID : "tcs100"
+
+// }
+
+// function accessUser(Employee){
+//   console.log(`User details are :
+//                The name of TCS Employee of user ${Employee.userName}                  
+//                and the userID of Employee is ${Employee.userID}`);              // function
+
+// }
+// accessUser(userOne);
+
+
+
+
+let arr = [10, 20, 30]
+console.log(arr);
+
+
+function accessArr(getValue){
+  return getValue[1]
+}
+console.log(accessArr(arr));
--- variables.js
+++ variables.js
@@ -31,3 +31,90 @@
 
 // console.log(a)
 
+
+// const _pi = 3.14;
+// let person = "mohit Shinde";
+// let answer = "yes I'm";
+
+// console.log(_pi, person, answer);                    // One line
+// console.log(`_pi =${_pi}, human = ${person}, ans = ${answer}`);               //Print with labels using template literals
+// console.log({_pi, person, answer});            //Print as an object
+
+
+// .......we can't do this .....
+// let a = 100;
+// let a = "jhon snow";
+
+// ...same as that we can do this in var...
+// var b = 100;
+// var b = "jhon snow";
+
+// console.log(b);
+
+
+// let c = 3;
+// {
+//     let c = 5;
+// }                                               //Redeclaring a variable inside a block will not redeclare the variable outside the block:
+
+// console.log(`Output for let is ${c}`);
+
+
+
+// var a = 2;
+// {
+//     var a = 5;
+// }                                                //Redeclaring a variable inside a block will also redeclare the variable outside the block:
+
+// console.log(`output for var is ${a}`);
+
+
+
+// carName = "BMW";
+// var carName;                                        //Meaning: You can use the variable before it is declared:
+// console.log(carName);
+
+
+// carName1 = "BMW";
+// let carName1;                                //Meaning: Using a let variable before it is declared will result in a ReferenceError:
+// console.log(carName1);
+
+
+
+// .............CONSTANT...........
+
+
+// const PI = 3.145456789876543;
+// const PI = 3.14;                                    //A variable defined with the const keyword cannot be reassigned
+// PI = PI + 10
+
+
+
+// const cars = ["BMW", "Audi", "VW"];
+// cars[0] = "Toyota";
+// cars.push("Honda");
+// console.log(cars);
+
+
+// let user = [];
+// user = [{fName : "Mohit",
+//         userAge : 25}];
+// user.push(user);
+// console.log(user);                                  //Pushin an object in array.
+
+
+const car = { type:"Fiat", model:"500", color:"white"}
+car.color = "red";                      // You can change a property:
+car.owner = "John Snow";                // You can add a property:
+console.log(car);
+
+
+
+
+
+
+
+
+
+
+
