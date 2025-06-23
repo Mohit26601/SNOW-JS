59352ea0bcfc5ab6b2fcbb5cfdb321fc4353d083
unknown <mohitshinde26601@gamil.com>
2025-06-23 20:43:26 +0530

    conditonalStatement

---
+++ conditionalStatement.js
@@ -0,0 +1,63 @@
+// conditional Statement.
+
+
+// if (condtion) {
+//     // logic
+
+// }else {
+//     //if condition is false
+// }
+
+
+// if (true) {
+//     console.log("condition is True");
+
+// }else{
+//     console.log("condition is False");
+
+// }
+
+
+// let age = 16;
+
+// if(age >= 18) {
+//     console.log("User can vote");
+
+// }else{
+//     console.log("User can't vote");
+    
+// }
+
+
+
+// let age = 11;
+
+// if(age >= 18) {
+//     console.log("User can vote");
+
+
+
+// }else if(age < 12){
+//     console.log("user is minor");
+    
+// }
+
+// else{
+//     console.log("User can't vote");
+    
+// }
+
+
+// let age = 22;
+
+// if (age >= 18) {
+//     console.log("user can vote");
+    
+// }
+
+
+
+//........Switch.......
+
+let t = Date()
+console.log();
