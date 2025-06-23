0ea9710b60ee109229c0cf9ff92dd9412ceb6c42
unknown <mohitshinde26601@gamil.com>
2025-06-24 02:47:54 +0530

    datatypes24june

--- datatypes.js
+++ datatypes.js
@@ -1,12 +1,14 @@
 // string
 
-// let firstName = "virat";
-// let lastName = "kohli";
+let firstName = "virat";
+let lastName = "kohli";
 
-// console.log(firstName);
-// console.log(lastName);
-
+console.log(firstName,lastName);
 
 // let a = 500;
-// b = 400;
+// {
+//     a = 400;
+// }
 //  console.log(a); 
+
+
